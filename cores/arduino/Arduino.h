b26011576f5395bcac3d56b0d48ef80b666a3f4d
LynnL4 <lht856@foxmail.com>
2020-05-07 14:40:28 +0800

    fixed: fixed conflicts between min max and other libraries

--- cores/arduino/Arduino.h
+++ cores/arduino/Arduino.h
@@ -98,10 +98,38 @@ void loop( void ) ;
 #ifdef abs
 #undef abs
 #endif // abs
+// undefine stdlib's abs if encountered
+#ifdef abs
+#undef abs
+#endif // abs
 
-/// conflict with STL min/max
-#define min(a,b) ((a)<(b)?(a):(b))
-#define max(a,b) ((a)>(b)?(a):(b))
+#ifdef __cplusplus
+  template<class T, class L> 
+  auto min(const T& a, const L& b) -> decltype((b < a) ? b : a)
+  {
+    return (b < a) ? b : a;
+  }
+
+  template<class T, class L> 
+  auto max(const T& a, const L& b) -> decltype((b < a) ? b : a)
+  {
+    return (a < b) ? b : a;
+  }
+#else
+#ifndef min
+#define min(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
+#endif
+#ifndef max
+#define max(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
+#endif
+#endif
+
 #define abs(x) ((x)>0?(x):-(x))
 #define constrain(amt,low,high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
 #define round(x)     ((x)>=0?(long)((x)+0.5):(long)((x)-0.5))
